796c9198d7b0bbb7099a2fa0cb4d0306053a2a56
Bauko Balazs <baukobalazs20@gmail.com>
2025-11-11 13:32:22 +0100

    login logout

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@ import {z} from 'zod'
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { auth, signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+import { resourceUsage } from 'process';
 
 const USE_MOCK = process.env.USE_MOCK_DATA === 'true';
 
@@ -124,6 +127,25 @@ export async function createInvoice(prevState : State,formdata: FormData){
       DELETE FROM invoices WHERE id = ${id}
       `;
       revalidatePath('/dashboard/invoices')
+    }
+  }
+
+  export async function authenticate(
+    prevState : string | undefined,
+    formData : FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if(error instanceof AuthError){
+        switch (error.type){
+          case 'CredentialsSignin': 
+          return 'Invalid credentials.';
+          default: 
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
     }
   }
 
