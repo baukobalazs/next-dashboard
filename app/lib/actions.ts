003b040d7bd01ccdc687aa2f357793f747ceb5b0
Bauko Balazs <baukobalazs20@gmail.com>
2025-11-12 09:33:34 +0100

    adsf

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -130,22 +130,22 @@ export async function createInvoice(prevState : State,formdata: FormData){
     }
   }
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
+  export async function authenticate(
+    prevState : string | undefined,
+    formData : FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if(error instanceof AuthError){
+        switch (error.type){
+          case 'CredentialsSignin': 
           return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
+          default: 
+            return 'Something went wrong.';
+        }
       }
+      throw error;
     }
-    throw error;
   }
-}
 
