2a4d75479be20d08c614b6941096900ee613c5a3
Bauko Balazs <baukobalazs20@gmail.com>
2025-11-12 09:45:41 +0100

    asdqwd

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,5 @@
 import type { NextAuthConfig } from 'next-auth';
+import { redirect } from 'next/navigation';
  
 export const authConfig = {
   pages: {
@@ -12,7 +13,7 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; 
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        redirect('dashboard')
       }
       return true;
     },
