cdb272ad2c434b91c0b1730889f4fd880b6ae4a3
Bauko Balazs <baukobalazs20@gmail.com>
2025-11-12 09:04:50 +0100

    loginxifx

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -67,7 +67,7 @@ export default function LoginForm() {
           </div>
         </div>
         <input type="hidden" name="redirectTo" value={callbackUrl} />
-        <Button className="mt-4 w-full">
+        <Button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div
@@ -75,6 +75,12 @@ export default function LoginForm() {
           aria-live="polite"
           aria-atomic="true"
         >
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
           {/* Add form errors here */}
         </div>
       </div>
