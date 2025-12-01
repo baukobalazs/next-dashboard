5de8a605b4d348e301b6c028a7ce1070f1aa5021
Bauko Balazs <baukobalazs20@gmail.com>
2025-12-01 13:01:58 +0100

    asd

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -775,6 +775,7 @@ export async function updateInvoiceStatus(invoiceId: string, status: string) {
     SET status = ${status}
     WHERE id = ${invoiceId}
   `;
+  revalidatePath("/dashboard")
 }
 
 
